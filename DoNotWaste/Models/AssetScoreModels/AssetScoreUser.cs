798911c61c2c7723d077447d285f26b9192fa049
domenico.cimmino <domenico.cimmino@lutech.it>
2024-06-07 16:45:57 +0200

    Add Configuration and UserModel

---
+++ DoNotWaste/Models/AssetScoreModels/AssetScoreUser.cs
@@ -0,0 +1,37 @@
+using Newtonsoft.Json;
+
+namespace DoNotWaste.Models.AssetScoreModels;
+
+public class AssetScoreUser
+{
+    /// <summary>
+    /// The unique identifier for this user, used in get and update requests.
+    /// </summary>
+    [JsonProperty("id")]
+    public int? Id { get; set; }
+
+    /// <summary>
+    /// Email address used for login, resetting passwords, and contacting the user
+    /// with simulation results.
+    /// </summary>
+    [JsonProperty("email")]
+    public string? Email { get; set; }
+
+    /// <summary>
+    /// Unique identifier for the organization this user belongs to.
+    /// </summary>
+    [JsonProperty("organization_id")]
+    public int? OrganizationId { get; set; }
+
+    /// <summary>
+    /// Date and time the user account was created in the Energy Asset Score application.
+    /// </summary>
+    [JsonProperty("created_at")]
+    public DateTime? CreatedAt { get; set; }
+
+    /// <summary>
+    /// Date and time the user account was updated in the Energy Asset Score application.
+    /// </summary>
+    [JsonProperty("updated_at")]
+    public DateTime? UpdatedAt { get; set; }
+}
